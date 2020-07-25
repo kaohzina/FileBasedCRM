5b3cc48e0557951587bf4fda81d67d94a910ca26
Z Kaoher <kaohzina@gmail.com>
2020-07-25 20:26:36 +0000

    If file is already made, program will request new file name.

--- FileBasedCRM.java
+++ FileBasedCRM.java
@@ -3,34 +3,32 @@ import java.io.File;
 import java.io.IOException;
 public class FileBasedCRM{
   public static void main(String[] args){
-    static void newfile{}
       Scanner user_input = new Scanner(System.in); 
-      String filename;
+      String filename, answer;
+      answer = "N";
+      while(answer.equalsIgnoreCase("N")){
       System.out.println("Give your file a name.");
       filename = user_input.nextLine(); 
       System.out.println("Is that a good file name? Y/N");
-      answer = user_input.nextLine();
-      
-  if (answer.equalsIgnoreCase("Y")){
-    try{ 
-      File myObj = new File(filename);
-    user_input.nextLine();
-    if (myObj.createNewFile()){
-      System.out.println("You created file" + filename);
-    }
-    else{
-      System.out.println("That file exists already.");
+        answer = user_input.nextLine();
+      if (answer.equalsIgnoreCase("Y")){
+        try{ 
+          File myObj = new File(filename);
+        if (myObj.createNewFile()){
+          System.out.println("You created file " + filename);
+          }
+        else{
+          System.out.println("That file exists already.");
+          answer = "N";
+          }
+        }
+        catch (IOException e){
+          System.out.println("That file exists already.");
+          e.printStackTrace();
+          answer = "N";  
+          }
+        }  
       }
     }
-    catch (IOException e){
-    System.out.println("That file exists already.");
-    e.printStackTrace();  
-      }
-  else{
-
   }
-    
   
-    }
-  }
-}
