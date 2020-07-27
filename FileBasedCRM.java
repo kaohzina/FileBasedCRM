8e3aa7894c2bf235bcd47e4a4e573aa8e65bf13c
Z Kaoher <kaohzina@gmail.com>
2020-07-27 02:05:54 +0000

    Allowed files to be created and filled

--- FileBasedCRM.java
+++ FileBasedCRM.java
@@ -8,34 +8,41 @@ import java.util.ArrayList;
 public class FileBasedCRM {
   public static void main(String[] args){
     Scanner user_input = new Scanner(System.in); 
-    String filename, answer, request;
+    String filename, answer, request, profile, file, text;
   int choice;
   Scanner sc = new Scanner(System.in);
-  System.out.println("1. Employee List");
-  System.out.println("2. Add Employee");
-  System.out.println("3. Remove Employee"); 
-  System.out.println("4. Edit Employee");
+  answer = "y";
+ while (answer.equalsIgnoreCase("Y")){
+  System.out.println("1. View File");
+  System.out.println("2. Add File");
+  System.out.println("3. Remove File"); 
+  System.out.println("4. Edit File");
+  System.out.println("5. Exit");
   System.out.println("Make a selection.");
   choice=sc.nextInt();
   switch(choice){
     case 1:
-      request ="Employee List";
+      request ="View File";
         break;
     case 2:
-      request ="Add Employee";
+      request ="Add File";
         break;
     case 3:
-      request ="Remove Employee";
+      request ="Remove File";
         break;
     case 4:
-      request ="Edit Employee";
+      request ="Edit File";
+        break;
+    case 5:
+      request ="Exit";
         break;
     default: 
       request ="Selection not listed";
+    
       }
     System.out.println("Loading " + request);
-
-   do {
+ 
+      if (request.equalsIgnoreCase("Add File")){
       System.out.println("Give your file a name.");
       filename = user_input.nextLine(); 
       System.out.println("Is that file properly spelt? Y/N");
@@ -50,6 +57,8 @@ public class FileBasedCRM {
           System.out.println("That file exists already.");
           answer = "N";
           }
+          System.out.println("Would you like to make another selection Y/N?");
+          answer = user_input.nextLine();
         }
         catch (IOException e){
           System.out.println("That file exists already.");
@@ -57,27 +66,60 @@ public class FileBasedCRM {
           answer = "N";  
           }
         }  
-      }
-      while (request.equals ("2"));
-      System.out.println("Would you like to read a file? Y/N"); 
-        answer = user_input.nextLine();
-      if (answer.equalsIgnoreCase("Y")){
-      System.out.println("Enter file name");
-      request = user_input.nextLine();
-    try {
-      File myObj = new File(request);
+      } 
+      
+
+
+      if (request.equalsIgnoreCase("View File")){
+      System.out.println("Enter file you would like to view.");
+      profile = user_input.nextLine();
+        try {
+      File myObj = new File(profile);
       Scanner myReader = new Scanner(myObj);
-      while (myReader.hasNextLine()) {
+        while (myReader.hasNextLine()) {
         String data = myReader.nextLine();
         System.out.println(data);
+          System.out.println("Would you like to make another selection Y/N?");
+          answer = user_input.nextLine();
+        }
+        myReader.close();
+        } catch (FileNotFoundException e) {
+          System.out.println("An error occurred.");
+          e.printStackTrace();
+          }
+        
       }
-      myReader.close();
-    } catch (FileNotFoundException e) {
+
+      if(request.equalsIgnoreCase("Remove File")){
+        System.out.println("What file would you like to remove");
+        
+      }
+
+
+
+      if(request.equalsIgnoreCase("Edit File")){
+        System.out.println("What file would you like to edit?");
+        file = user_input.nextLine();
+        System.out.println("You may make an entry.");
+        text = user_input.nextLine();
+      try {
+      FileWriter myWriter = new FileWriter(file);
+      myWriter.write(text);
+      myWriter.close();
+      System.out.println("Successfully wrote to the file.");
+    } catch (IOException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
     }
+      }
+      if(request.equalsIgnoreCase("Exit")){
+        System.out.println("Have a good day.");
+        answer = "n";
+      }
+ }
 
-    }
+
   }
 }
+
 
